0ebc4aac695534ec89b54735dc9910d784178a47
Sourcery AI <>
2022-04-21 12:59:49 +0000

    'Refactored by Sourcery'

--- pre_treatment.py
+++ pre_treatment.py
@@ -2,7 +2,7 @@ def treat_input(data):
     """
     Treats the input json to keep it in the same format as the coeficcients
     """
-    treated_data = dict()
+    treated_data = {}
     for key, value in data.items():
         if key[0] == 'Q':
             treated_data[value] = 1
